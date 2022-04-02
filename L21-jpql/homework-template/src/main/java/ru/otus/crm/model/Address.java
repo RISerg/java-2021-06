b79604e85d04a84668b3d73c2d89eed06fd561f8
Рогачев Илья Сергеевич <rogachev_is@magnit.ru>
2022-04-03 00:56:05 +0300

    ДЗ 21. Hibernate JPQL

---
+++ L21-jpql/homework-template/src/main/java/ru/otus/crm/model/Address.java
@@ -0,0 +1,33 @@
+package ru.otus.crm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address implements Cloneable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    public Address(String street) {
+        this.id = null;
+        this.street = street;
+    }
+
+    @Override
+    public Address clone() {
+        return new Address(this.id, this.street);
+    }
+}
--- L21-jpql/homework-template/src/test/java/ru/otus/core/repository/DataTemplateHibernateTest.java
+++ L21-jpql/homework-template/src/test/java/ru/otus/core/repository/DataTemplateHibernateTest.java
@@ -3,7 +3,11 @@ package ru.otus.core.repository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.otus.base.AbstractHibernateTest;
+import ru.otus.crm.model.Address;
 import ru.otus.crm.model.Client;
+import ru.otus.crm.model.Phone;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,7 +17,9 @@ class DataTemplateHibernateTest extends AbstractHibernateTest {
     @DisplayName(" корректно сохраняет, изменяет и загружает клиента по заданному id")
     void shouldSaveAndFindCorrectClientById() {
         //given
-        var client = new Client("Вася");
+        var address = new Address("str1");
+        var phone = new Phone("123");
+        var client = new Client("Вася", phone, List.of(address));
 
         //when
         var savedClient = transactionManager.doInTransaction(session -> {
@@ -24,6 +30,8 @@ class DataTemplateHibernateTest extends AbstractHibernateTest {
         //then
         assertThat(savedClient.getId()).isNotNull();
         assertThat(savedClient.getName()).isEqualTo(client.getName());
+        assertThat(savedClient.getPhone()).isEqualTo(client.getPhone());
+        assertThat(savedClient.getAddressList()).isEqualTo(client.getAddressList());
 
         //when
         var loadedSavedClient = transactionManager.doInTransaction(session ->
@@ -36,6 +44,8 @@ class DataTemplateHibernateTest extends AbstractHibernateTest {
         //when
         var updatedClient = savedClient.clone();
         updatedClient.setName("updatedName");
+        updatedClient.setPhone(new Phone("456"));
+        updatedClient.setAddressList(List.of(new Address("str2")));
         transactionManager.doInTransaction(session -> {
             clientTemplate.update(session, updatedClient);
             return null;
